1cef7ac5c7d83b70ae34bca2740a68e1b251c886
sowmik-sec <sowmik.sec@gmail.com>
2024-08-16 19:40:12 +0600

    connect to mongodb and remove node_modules folder

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const port = 5000;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xgh8h2c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Share bite server is running!");
