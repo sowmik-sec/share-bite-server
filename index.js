493e45a596b50d3bc9065c9fbcd9b6a642670333
sowmik-sec <sowmik.sec@gmail.com>
2024-08-18 12:06:44 +0600

    verify jwt token

--- index.js
+++ index.js
@@ -16,6 +16,20 @@ const client = new MongoClient(uri, {
   },
 });
 
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorized access" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "Unauthorized access" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
+
 const cookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
