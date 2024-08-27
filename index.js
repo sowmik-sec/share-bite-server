e0496c754269b348ebe50a908d4929ea6897cb5b
sowmik-sec <sowmik.sec@gmail.com>
2024-08-27 12:42:40 +0600

    make jwt work

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 var cors = require("cors");
 const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const app = express();
 
 require("dotenv").config();
@@ -10,9 +11,11 @@ const port = 5000;
 app.use(
   cors({
     origin: ["http://localhost:5173"],
+    credentials: true,
   })
 );
 app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xgh8h2c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -28,11 +31,11 @@ const client = new MongoClient(uri, {
 const verifyToken = async (req, res, next) => {
   const token = req.cookies?.token;
   if (!token) {
-    return res.status(401).send({ message: "Unauthorized access" });
+    return res.status(401).send({ message: "unauthorized access" });
   }
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) {
-      return res.status(401).send({ message: "Unauthorized access" });
+      return res.status(401).send({ message: "unauthorized access" });
     }
     req.user = decoded;
     next();
