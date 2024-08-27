2a832ca0f9a99d935bc9b7392c418d9c4ba3bc39
sowmik-sec <sowmik.sec@gmail.com>
2024-08-27 13:01:47 +0600

    deploy to vercel

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ const port = 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://sharebite-9ad0a.web.app",
+      "https://sharebite-9ad0a.firebaseapp.com",
+    ],
     credentials: true,
   })
 );
